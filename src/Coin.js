a325518243468f9121364147c4e4864a864868fc
DevelopShin <shin.github@gmail.com>
2021-12-23 23:31:51 +0900

    대충 완료

--- src/Coin.js
+++ src/Coin.js
@@ -1,19 +1,28 @@
 import React from 'react'
-
+import './Coin.css'
 function Coin(props) {
+  const coin = props.coin
   return (
     <div className='coin-container'>
       <div className="coin-row">
-        <img src={props.coin.image} alt="crypto" />
-        <h1>{props.coin.name}</h1>
-        <p className="coin-symbol">{props.coin.symbol}</p>
+        <div className="coin">
+          <img src={coin.image} alt="crypto" />
+          <h1>{coin.name}</h1>
+          <p className="coin-symbol">{coin.symbol}</p>
+
+        </div>
+        <div className="coin-data">
+          <p className="coin-price"> ${coin.current_price}</p>
+          <p className="coin-volume"> ${coin.market_cap.toLocaleString()}</p>
+          {coin.price_change_percentage_24h < 0
+            ? <p className='coin-percent blue'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+            : <p className='coin-percent red'>{coin.price_change_percentage_24h.toFixed(2)}%</p>}
+          <p className='coin-marketcap'>
+            Mt Cap: ${coin.market_cap}
+          </p>
+        </div>
       </div>
-      <div className="coin-data">
-        <p className="coin-price">${props.coin.current_price}</p>
-        <p className="coin-volume">${props.coin.market_cap.toLocaleString()}</p>
-        
-      </div>
-      
+
     </div>
   )
 }
