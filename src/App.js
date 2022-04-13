f8428a887f51ece97cecf1fb24863bed2119c3b1
DevelopShin <shin.github@gmail.com>
2022-04-13 23:40:27 +0900

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import CoinView from './components/CoinView';
+import { BrowserRouter } from 'react-router-dom';
 function App() {
 
   return (
-    <CoinView basename={process.env.PUBLIC_URL}/>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <CoinView />
+    
+    </BrowserRouter>
   );
 }
 
