a73904c8fd0d4ef6b8017ec197b48bb4f60003ec
DevelopShin <shin.github@gmail.com>
2022-04-14 00:41:44 +0900

    pavicon &&title

--- src/components/CoinView.js
+++ src/components/CoinView.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Coin from './Coin';
 import './CoinView.css'
+// const icon = require('../../public/logo-gif.webp')
+// import imgUrl from './logo-gif.webp'
 const API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=jpy&order=market_cap_desc&per_page=100&page=1&sparkline=false"
 
 function CoinView() {
@@ -22,6 +24,7 @@ function CoinView() {
   return (
     <div className="coin-app">
       <div className='coin-search'>
+      <img width="50px" alt='logoimages' src={process.env.PUBLIC_URL + '/logo-gif.webp'}/>
         <h1 className="coin-text">Search a currency</h1>
         <form>
           <input type="text" placeholder='Search' className='coin-input' onChange={searchHandler} />
